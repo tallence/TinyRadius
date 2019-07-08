7154892c61ac48665276796397c28695f87ad9a5
Christian Harnisch <c.harnisch@tallence.com>
2019-07-08 13:44:52 +0200

    Split IPv6PrefixAttribute tests, add cases for invalid prefix length

--- src/test/java/org/tinyradius/attribute/Ipv6PrefixAttributeTest.java
+++ src/test/java/org/tinyradius/attribute/Ipv6PrefixAttributeTest.java
@@ -1,9 +1,8 @@
 package org.tinyradius.attribute;
 
-import com.sun.org.apache.xerces.internal.impl.dv.util.HexBin;
 import org.apache.commons.codec.DecoderException;
-import org.junit.Test;
 import org.apache.commons.codec.binary.Hex;
+import org.junit.Test;
 import org.tinyradius.util.RadiusException;
 
 import static org.junit.Assert.assertEquals;
@@ -11,17 +10,34 @@ import static org.junit.Assert.assertEquals;
 public class Ipv6PrefixAttributeTest {
 
 	@Test
-	public void test() throws DecoderException, RadiusException {
-	     Ipv6PrefixAttribute attr = new Ipv6PrefixAttribute(97, "2003:4:12a0:500::/56");
-	    assertEquals("2003:4:12a0:500:0:0:0:0/56", attr.getAttributeValue());
-	    byte[] data = attr.getAttributeData();
+	public void test_fromString() {
+		Ipv6PrefixAttribute attr = new Ipv6PrefixAttribute(97, "2003:4:12a0:500::/56");
+		assertEquals("2003:4:12a0:500:0:0:0:0/56", attr.getAttributeValue());
+	}
 
-            Hex hex = new Hex();
-	    data = hex.decodeHex("610c0038200300041285094a");
+	@Test
+	public void test_fromBytes() throws DecoderException, RadiusException {
+		byte[] data = Hex.decodeHex("610c0038200300041285094a");
 
-	    Ipv6PrefixAttribute attr2 = new Ipv6PrefixAttribute();
-	    attr2.readAttribute(data,0,data.length);
-	    assertEquals("2003:4:1285:94a:0:0:0:0/56", attr2.getAttributeValue());
+		Ipv6PrefixAttribute attr = new Ipv6PrefixAttribute();
+		attr.readAttribute(data, 0, data.length);
+		assertEquals("2003:4:1285:94a:0:0:0:0/56", attr.getAttributeValue());
+	}
+
+	@Test(expected = RadiusException.class)
+	public void test_tooFewBytes_throwsException() throws Exception {
+		byte[] data = Hex.decodeHex("610c00");
+
+		Ipv6PrefixAttribute attr = new Ipv6PrefixAttribute();
+		attr.readAttribute(data, 0, data.length);
+	}
+
+	@Test(expected = RadiusException.class)
+	public void test_tooManyBytes_throwsException() throws Exception {
+		byte[] data = Hex.decodeHex("610c0038200300041285094a38a020b4224789237a");
+
+		Ipv6PrefixAttribute attr = new Ipv6PrefixAttribute();
+		attr.readAttribute(data, 0, data.length);
 	}
 
 }
